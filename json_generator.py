71ab887487ee692deeda61f1cbd5c5b2904f564a
Marc Prengemann <marcprengemann@web.de>
2015-09-09 13:02:53 +0200

    Included script to generate directory of icon packs

---
+++ json_generator.py
@@ -0,0 +1,75 @@
+import json
+import os
+import io
+from itertools import izip, islice
+from os.path import join
+
+root = 'src/main/assets/'
+
+
+def handle_android_icons():
+    iconpack_root = join(root, "android_icons")
+    colors = os.listdir(iconpack_root)
+    firstcolordir = join(iconpack_root, colors[0])
+    resolutions = os.listdir(firstcolordir)
+    assets = os.listdir(join(firstcolordir, resolutions[0]))
+    packdata = {'pack': 'android_icons', 'path': iconpack_root}
+    assetdata = []
+    for asset in assets:
+        data = {'name': os.path.splitext(asset)[0],
+                'category': 'all',
+                'resolutions': resolutions,
+                'colors': colors,
+                'sizes': ['18dp']}
+        assetdata.append(data)
+
+    packdata['assets'] = assetdata
+    return packdata
+
+
+def extract_data(icon_file_name):
+    file_name = os.path.splitext(icon_file_name)[0]
+    splits = file_name.split('_')
+    size = splits[-1]
+    splits.pop()
+    color = splits[-1]
+    splits.pop()
+    name = '_'.join(splits)
+    return name, color, size
+
+
+def handle_material_icons():
+    iconpack_root = join(root, "material_icons")
+    categories = os.listdir(iconpack_root)
+    packdata = {'pack': 'material_icons', 'path': iconpack_root}
+    assetdata = []
+    for category in categories:
+        category_root = join(iconpack_root, category)
+        resolutions = os.listdir(category_root)
+        raw_assets = [extract_data(f) for f in os.listdir(join(category_root, resolutions[0]))]
+        colors = []
+        sizes = []
+        for current_item, next_item in izip(raw_assets, islice(raw_assets, 1, None)):
+            colors.append(current_item[1])
+            sizes.append(current_item[2])
+            if current_item[0] != next_item[0]:
+                data = {'name': current_item[0],
+                        'category': category,
+                        'resolutions': resolutions,
+                        'colors': sorted(set(colors)),
+                        'sizes': sorted(set(sizes))}
+                assetdata.append(data)
+                colors = []
+                sizes = []
+                continue
+
+    packdata['assets'] = assetdata
+    return packdata
+
+
+android_icons_data = handle_android_icons()
+material_icons_data = handle_material_icons()
+packs = {'packs': [android_icons_data, material_icons_data]}
+
+with io.open(join(root, 'content.json'), 'w', encoding='utf-8') as f:
+    f.write(unicode(json.dumps(packs, ensure_ascii=False)))

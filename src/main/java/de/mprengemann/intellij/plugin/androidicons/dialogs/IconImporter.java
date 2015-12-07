4aed5101bd07e9bf7b7cf7ebcb3b95603a199d58
Marc Prengemann <marcprengemann@web.de>
2015-12-07 11:31:07 +0100

    Improved hint for search

--- src/main/java/de/mprengemann/intellij/plugin/androidicons/dialogs/IconImporter.java
+++ src/main/java/de/mprengemann/intellij/plugin/androidicons/dialogs/IconImporter.java
@@ -52,6 +52,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.beans.PropertyChangeEvent;
@@ -214,6 +216,12 @@ public class IconImporter extends DialogWrapper implements IconsImporterObserver
                 return element instanceof ImageAsset ? ((ImageAsset) element).getName() : "";
             }
         };
+        searchField.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                comboboxSpeedSearch.showPopup();
+            }
+        });
         searchField.addItemListener(searchFieldListener);
     }
 
